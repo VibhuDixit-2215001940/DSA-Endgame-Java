19941a1e166e959f18967b3ca025a2315d292693
Vibhu Dixit <vibhudixit.2215001940@gmail.com>
2025-02-06 19:37:57 +0530

    add ok

--- Lecture/Day7/SieveOfEratosthenes.java
+++ Lecture/Day7/SieveOfEratosthenes.java
@@ -2,22 +2,22 @@ import java.util.Arrays;
 
 public class SieveOfEratosthenes {
     public static void sieve(int n) {
-        boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
+        int[] isPrime = new int[n + 1];
+        Arrays.fill(isPrime, 1);
 
-        isPrime[0] = isPrime[1] = false; // 0 and 1 are not prime
+        isPrime[0] = isPrime[1] = 0; // 0 and 1 are not prime
 
         for (int i = 2; i * i <= n; i++) {
-            if (isPrime[i]) {
+            if (isPrime[i] == 1) {
                 for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false;
+                    isPrime[j] = 0;
                 }
             }
         }
 
         // Printing all prime numbers
         for (int i = 2; i <= n; i++) {
-            if (isPrime[i]) {
+            if (isPrime[i] == 1) {
                 System.out.print(i + " ");
             }
         }
