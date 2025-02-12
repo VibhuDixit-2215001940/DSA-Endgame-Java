2eb8ec3de2ae857d36195dd02f794f981209cbe5
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-02-12 08:28:29 +0530

    Glass Animals - Heat Waves

---
+++ POTD/Max Sum of a Pair With Equal Sum of Digits.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int findSum(int number) {
+        return number == 0 ? 0 : number % 10 + findSum(number / 10);
+    }
+
+    public int maximumSum(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int num : nums) {
+            int sum = findSum(num);
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(num);
+        }
+        int maxSum = -1;
+        for (List<Integer> arr : map.values()) {
+            if (arr.size() >= 2) {
+                int firstMax = 0, secondMax = 0;
+                for (int x : arr) {
+                    if (x > firstMax) {
+                        secondMax = firstMax;
+                        firstMax = x;
+                    } else if (x > secondMax)
+                        secondMax = x;
+                }
+                maxSum = Math.max(maxSum, firstMax + secondMax);
+            }
+        }
+        return maxSum;
+    }
+}
