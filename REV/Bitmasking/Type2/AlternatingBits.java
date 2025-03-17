017ae9f71b3523e059ffa3d20bfecad53e196358
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-17 20:24:38 +0530

    simplify

--- REV/Bitmasking/Type2/AlternatingBits.java
+++ REV/Bitmasking/Type2/AlternatingBits.java
@@ -6,7 +6,7 @@ public class AlternatingBits {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int check = n + (n >> 1); // Sum of n and n shifted right
+        int check = n + (n >> 1); // Sum of n and n/2
         System.out.println((check & (check + 1)) == 0 ? "True" : "False");
         sc.close();
     }
