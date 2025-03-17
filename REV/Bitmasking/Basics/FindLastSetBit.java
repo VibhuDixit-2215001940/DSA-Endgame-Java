bd8572a6ef0cb06d84ca82ac57ba34ab80e06910
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-17 20:35:43 +0530

    add FindLastSetBits

--- REV/Bitmasking/Basics/FindLastSetBit.java
+++ REV/Bitmasking/Basics/FindLastSetBit.java
@@ -1,15 +1,12 @@
 package Basics;
 
-public class FindLastSetBit {
-    public static void main(String[] args) {
-        int n = 10;
-        int lastBit = findLastSetBit(n);
-        System.out.println(lastBit);
-    }
+import java.util.*;
 
-    public static int findLastSetBit(int n) {
-        if (n == 0)
-            return 0; // No set bit in 0
-        return (int) (Math.log(n & -n) / Math.log(2)) + 1;
+public class FindLastSetBit {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n & -n); // Extracts the last set bit
+        sc.close();
     }
 }
