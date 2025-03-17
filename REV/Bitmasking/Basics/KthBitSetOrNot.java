63a0b3008a26d9393646e88ed528bc0f5b4ffaf2
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-17 06:42:03 +0530

    add nothing

--- REV/Bitmasking/Basics/KthBitSetOrNot.java
+++ REV/Bitmasking/Basics/KthBitSetOrNot.java
@@ -4,7 +4,7 @@ public class KthBitSetOrNot {
     public static void main(String[] args) {
         int n = 4; // Binary: 100
         int k = 3; // Check if the 3rd bit (1-based) is set
-        System.out.println(check(n, k)); // Output: true
+        System.out.println(check(n, k));
     }
 
     public static boolean check(int n, int k) {
