ad25badba34007a90306ca4dca15d8e37751ac07
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-17 20:16:14 +0530

    add AlternatingBits

---
+++ REV/Bitmasking/Type2/AlternatingBits.java
@@ -0,0 +1,13 @@
+package Type2;
+
+import java.util.*;
+
+public class AlternatingBits {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int check = n + (n >> 1); // Sum of n and n shifted right
+        System.out.println((check & (check + 1)) == 0 ? "True" : "False");
+        sc.close();
+    }
+}
